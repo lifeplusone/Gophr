3e15ab8f331d24211f9af6a6eef47263f110f7f3
Khalil Van Alphen <khalilvanalphen@kva-macbook.youilabs.local>
2016-08-30 14:30:17 -0400

    Firebase initialization

--- app/src/main/java/com/example/khalilvanalphen/gophr/MainActivity.java
+++ app/src/main/java/com/example/khalilvanalphen/gophr/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.khalilvanalphen.gophr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFetch;
+
+    Firebase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://gophr-c8962.firebaseio.com/Items");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        btnFetch = (Button) findViewById(R.id.btn_fetch);
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                btnFetch.setText(text);
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
