1078d3c71cef730835daf27553059709faf3be8c
kanguilla <khalil.va@gmail.com>
2016-09-06 14:29:19 -0400

    Added registering if no authentication

--- app/src/main/java/com/example/khalilvanalphen/gophr/LoginActivity.java
+++ app/src/main/java/com/example/khalilvanalphen/gophr/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText inputEmail, inputPassword;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
-    private Button btnSignup, btnLogin, btnReset;
+    private Button btnLogin, btnReset;
 
 
     public void hideKeyboard(View view) {
@@ -55,7 +55,6 @@ public class LoginActivity extends AppCompatActivity {
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnSignup = (Button) findViewById(R.id.btn_signup);
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnReset = (Button) findViewById(R.id.btn_reset_password);
 
@@ -77,13 +76,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-            }
-        });
-
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,7 +86,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = inputEmail.getText().toString();
+                final String email = inputEmail.getText().toString();
                 final String password = inputPassword.getText().toString();
 
                 if (TextUtils.isEmpty(email)) {
@@ -120,6 +112,25 @@ public class LoginActivity extends AppCompatActivity {
                                 progressBar.setVisibility(View.GONE);
                                 if (!task.isSuccessful()) {
                                     Toast.makeText(LoginActivity.this, "auth failed", Toast.LENGTH_LONG).show();
+
+                                    auth.createUserWithEmailAndPassword(email, password)
+                                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                                    // the auth state listener will be notified and logic to handle the
+                                                    // signed in user can be handled in the listener.
+                                                    progressBar.setVisibility(View.GONE);
+                                                    if (!task.isSuccessful()) {
+                                                        Toast.makeText(LoginActivity.this, "register failed", Toast.LENGTH_LONG).show();
+                                                    } else {
+                                                        Intent intent = new Intent(LoginActivity.this, BrowseActivity.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                }
+                                            });
+
                                 } else {
                                     Intent intent = new Intent(LoginActivity.this, BrowseActivity.class);
                                     startActivity(intent);
@@ -129,5 +140,6 @@ public class LoginActivity extends AppCompatActivity {
                         });
             }
         });
+
     }
 }
