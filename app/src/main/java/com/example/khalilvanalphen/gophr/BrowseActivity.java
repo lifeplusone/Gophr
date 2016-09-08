c0b8f3d6eac3e53a88015472a9d9fd62e2f40c0c
Khalil Van Alphen <khalilvanalphen@kva-macbook.youilabs.local>
2016-09-08 12:42:20 -0400

    Added side bar to the main screen

--- app/src/main/java/com/example/khalilvanalphen/gophr/BrowseActivity.java
+++ app/src/main/java/com/example/khalilvanalphen/gophr/BrowseActivity.java
@@ -3,7 +3,16 @@ package com.example.khalilvanalphen.gophr;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -17,10 +26,9 @@ import com.firebase.client.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
-public class BrowseActivity extends AppCompatActivity {
-    Button btnDebug;
+public class BrowseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Button btnCreate, btnDebug;
     ListView listView, listViewExtra;
 
     GphTaskAdapter adapter, adapterExtra;
@@ -33,28 +41,52 @@ public class BrowseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_browse);
         Firebase.setAndroidContext(this);
         mRef = new Firebase("https://gophr-c8962.firebaseio.com/");
+
+
+        //new stuff
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void onStart(){
         super.onStart();
+        btnCreate = (Button) findViewById(R.id.btn_create);
         btnDebug = (Button) findViewById(R.id.btn_debug);
 
-        btnDebug.setOnClickListener(new View.OnClickListener() {
+        btnCreate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), TaskCreationActivity.class);
                 startActivityForResult(i, 1);
             }
         });
 
-        ArrayList<GphTask> data = new ArrayList<GphTask>();
-
+        ArrayList<GphTask> data = new ArrayList<>();
+        ArrayList<GphTask> dataExtra = new ArrayList<>();
         listView = (ListView) findViewById(R.id.list_view);
         adapter = new GphTaskAdapter(this, R.layout.task_list_row_item, data);
         listView.setAdapter(adapter);
 
         listViewExtra = (ListView) findViewById(R.id.list_view2);
-        adapterExtra = new GphTaskAdapter(this, R.layout.task_list_row_item, data);
+        adapterExtra = new GphTaskAdapter(this, R.layout.task_list_row_item, dataExtra);
         listViewExtra.setAdapter(adapterExtra);
 
 
@@ -112,16 +144,70 @@ public class BrowseActivity extends AppCompatActivity {
         }
     }
     public void updateList(DataSnapshot data){
-        Iterator<DataSnapshot> id = data.getChildren().iterator();
-        while (id.hasNext()){
-            GphTask task = id.next().getValue(GphTask.class);
-            if (task.getOwner().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+        for (DataSnapshot dataSnapshot : data.getChildren()) {
+            GphTask task = dataSnapshot.getValue(GphTask.class);
+            if (!task.getOwner().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
                 adapter.add(task);
-            }else{
+            } else {
                 adapterExtra.add(task);
             }
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
